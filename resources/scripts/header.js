d0b0a5a8ad1aed9e24058fc9c3ca96a81b36e07e
mrathgeber <mjrathgeber@gmail.com>
2023-11-17 00:43:39 -0500

    updated information architecture
    moved all html to root, added styles, scripts, and images folders for better organization

---
+++ resources/scripts/header.js
@@ -0,0 +1,11 @@
+$(function () {
+   var output = "";
+
+   output += "<div class=header>";
+   output += "<a href='index.html'>";
+   output += "<img src='../resources/trudge-152x152.png' class='favicon' "
+
+   output += "</div>";
+
+   $("body").append(output);
+});
--- scripts/header.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-   var output = "";
-   const currentUrl = window.location.href;
-
-   output += "<a href="
-
-   $(".header").html(output);
-});
