e3e69565499333bc1479ff029d81fbf91bf5bd0d
mrathgeber <mjrathgeber@gmail.com>
2024-04-12 16:35:31 -0400

    header edits

--- resources/scripts/header.js
+++ resources/scripts/header.js
@@ -6,10 +6,10 @@ $(function () {
 
    // Trudge favicon
    output += "<a href='index.html'>" +
-      "<img src='resources/images/trudge-152x152.png' class='favicon' alt='Trudge Logo'>" +
+      "<img src='resources/images/strudge-logo.png' class='favicon' alt='Trudge Logo'>" +
       "</a>";
 
-   output += "<div id='header'>";
+   output += "<div class='header'>";
 
    // Header title
    output += "<div>" +
@@ -28,7 +28,20 @@ $(function () {
    output += "</div>";
    output += "</div>";
 
-   output += "<div id='sticky'><p>hi</p></div>"
+   output += "<div id='sticky' class='header'>" +
+      "<div>" +
+      "<a href='index.html'>" +
+      "<h1 id='title'>RPI ULTIMATE</h1>" +
+      "</a>" +
+      "</div>";
+
+   output += "<nav>" +
+   "<p class='nav-element'><a href='ultimate101.html'>Ultimate 101</a></p>" +
+   "<p class='nav-element'><a href='officers.html'>Meet The Crew</a></p>" +
+   "<p class='nav-element'><a href='about.html'>About</a></p>" +
+   "</nav>";
+   
+   output+= "</div>";
 
    // output += "<img class='home-img' src='resources/images/trudge-capital.jpg' alt='Trudge at Capital'>";
 
